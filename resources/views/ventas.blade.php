a2bd4491550e0c3ed29fcabba1385cdd77362fec
RobertoAngelCetzalAlvarez <90730437+RobertoAngelCetzalAlvarez@users.noreply.github.com>
2021-12-06 17:47:04 -0600

    punto de ventas
    interface inicial

--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.master')
 @section('titulo','VENTAS') 
 @section('contenido')
+<div id='apiVenta'>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" placeholder="Escriba el codigo del producto" aria-label="Recipent's username" aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<button class="btn btn-outline-secondary" type="button">Button</button>
+						</div>
+					
+				</div>
+			</div>
 
+		</div>
+	</div>
+	
+</div>
 
 @endsection
 
-@push('scripts')
+@push('scripts') 
 <script type="text/javascript" src="js/vue-resource.js"></script>
-<script type="text/javascript" src="js/apis/apiMascota.js"></script>
+<script type="text/javascript" src="js/apis/apiVenta.js"></script>
 @endpush 
